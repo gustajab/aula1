bb7b47b89d19016dc954394fa92e25897ce63ff0
Gustavo Rajab <122064946+gustajab@users.noreply.github.com>
2023-04-11 15:52:17 -0300

    update usuarios

--- models/Model.php
+++ models/Model.php
@@ -67,6 +67,17 @@ class Model
     }
     public function update($data, $id){
 
+        // Remove indice 'id' do $data
+        unset($data['id']);
+
+        $sql = "UPDATE {$this->table}";
+        $sql.= ' SET ' . $this->sql_fields($data);
+        $sql.= ' WHERE id = :id';
+
+        $data['id'] = $id;
+
+        $upd = $this->conex->prepare($sql);
+        $upd->execute($data);
     }
 
     private function sql_fields($data){
